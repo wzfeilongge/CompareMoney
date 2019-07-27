c481d88bf0b7dd9094886b5b62162eaa428bdb85
wzfeilongge <494379797@qq.com>
2019-07-27 17:03:25 +0800

    jwt尚未完成

---
+++ CompareMoney.Core.Api/JwtHelper/JwtTokenAuth.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CompareMoney.Core.Api.JwtHelper
+{
+    public class JwtTokenAuth
+    {
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly RequestDelegate _next;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="next"></param>
+        public JwtTokenAuth(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+
+        private void PreProceed(HttpContext next)
+        {
+            //Console.WriteLine($"{DateTime.Now} middleware invoke preproceed");
+            //...
+        }
+        private void PostProceed(HttpContext next)
+        {
+            //Console.WriteLine($"{DateTime.Now} middleware invoke postproceed");
+            //....
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="httpContext"></param>
+        /// <returns></returns>
+        public Task Invoke(HttpContext httpContext)
+        {
+            PreProceed(httpContext);
+
+
+            //检测是否包含'Authorization'请求头
+            if (!httpContext.Request.Headers.ContainsKey("Authorization"))
+            {
+                PostProceed(httpContext);
+
+                return _next(httpContext);
+            }
+            //var tokenHeader = httpContext.Request.Headers["Authorization"].ToString();
+            var tokenHeader = httpContext.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+
+            try
+            {
+                if (tokenHeader.Length >= 128)
+                {
+                    //Console.WriteLine($"{DateTime.Now} token :{tokenHeader}");
+                    TokenModelJwt tm = JwtHelpers.SerializeJwt(tokenHeader);
+
+                    //授权
+                    var claimList = new List<Claim>();
+                    var claim = new Claim(ClaimTypes.Role, tm.Role);
+                    claimList.Add(claim);
+                    var identity = new ClaimsIdentity(claimList);
+                    var principal = new ClaimsPrincipal(identity);
+                    httpContext.User = principal;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{DateTime.Now} middleware wrong:{e.Message}");
+            }
+
+
+            PostProceed(httpContext);
+
+
+            return _next(httpContext);
+        }
+
+
+
+
+
+    }
+
+   // 这里定义一个中间件Helper，主要作用就是给当前模块的中间件取一个别名
+    public static class MiddlewareHelpers
+    {
+        public static IApplicationBuilder UseJwtTokenAuth(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<JwtTokenAuth>();
+        }
+    }
+}
