7c2c43713a638790be9e6c4badf70e4291a3985e
wzfeilongge <494379797@qq.com>
2019-07-31 17:18:10 +0800

    11

---
+++ CompareMoney.Core.Api/Controllers/DownLoadController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CompareMoney.Core.Api.ControllersModels;
+using CompareMoney.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompareMoney.Core.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DownLoadController : ControllerBase
+    {
+
+        public readonly DownLoadInterface _downLoadInterface;
+        public DownLoadController(DownLoadInterface downLoadInterface)
+        {
+            _downLoadInterface = downLoadInterface;
+        }
+
+
+
+        /// <summary>
+        /// 下载第三方数据到我方数据库
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [HttpGet("DwonLoadTosql", Name = "DwonLoadTosql")]
+       public async Task<IActionResult> DwonLoadTosql([FromBody]BillDataModel request) {
+
+            var Count = await _downLoadInterface.GetFXStmtLines(request.BillDate);
+
+
+            return Ok(new SucessModelCount(Count));
+
+        }
+
+
+
+
+
+
+
+    }
+}
