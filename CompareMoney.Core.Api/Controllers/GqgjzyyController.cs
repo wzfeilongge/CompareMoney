beb22d7850a4a8c139e608824befdccd456756d2
wzfeilongge <494379797@qq.com>
2019-09-05 10:01:26 +0800

    CompareMoney.Core.Api/Controllers/GqgjzyyController.cs

--- CompareMoney.Core.Api/Controllers/GqgjzyyController.cs
+++ CompareMoney.Core.Api/Controllers/GqgjzyyController.cs
@@ -20,27 +20,20 @@ namespace CompareMoney.Core.Api.Controllers
     [ApiController]
     public class GqgjzyyController : ControllerBase
     {
-
-
         /// <summary>
         /// 获得贵黔国际总医院Api.json 文件
         /// </summary>
         [HttpGet("GetJsonFile", Name = ("GetJsonFile"))]
-        public IActionResult GetJsonFile() {
-
+        public IActionResult GetJsonFile()
+        {
             //var config = new ConfigurationBuilder()
             // .SetBasePath(Directory.GetCurrentDirectory())
             // .AddJsonFile("postman_collection.json", optional: true, reloadOnChange: true)
             // .Build();
-
             //var list = config.Providers.FirstOrDefault();
-
             //var dyobj = (dynamic)list;
             //// var soure = dyobj.Source;
-            // var obj = dyobj.Data;
-
-            dynamic m = new ExpandoObject();
-        
+            // var obj = dyobj.Data;       
             string value = string.Empty;
             string jsonfile = "postman_collection.json";//File
             using (System.IO.StreamReader file = System.IO.File.OpenText(jsonfile))
